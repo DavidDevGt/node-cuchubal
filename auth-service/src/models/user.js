de431f1154ef70e185b7f3c2f1e1f7ba925195ad
DavidDevGt <josuedavidvl18@gmail.com>
2024-02-22 14:02:27 -0600

    Añadir modelo de usuario y rutas de autenticación

--- auth-service/src/models/user.js
+++ auth-service/src/models/user.js
@@ -1,3 +1,31 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize('mysql://user:password@host:port/database');
 
+const User = sequelize.define('User', {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    role: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    is_active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    }
+}, {
+    // No se
+})
+
+module.exports = User;
